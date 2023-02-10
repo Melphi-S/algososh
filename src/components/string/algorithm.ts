2fa9871bf525e95c0b0dff051c649b3f0f1de019
Yury Torgashin <imaginarylight@list.ru>
2023-02-10 20:50:12 +0300

    добавить prettier

--- src/components/string/algorithm.ts
+++ src/components/string/algorithm.ts
@@ -1,16 +1,19 @@
 import { swap } from "../../utils/utils";
-import { TCharElement} from "./string";
+import { TCharElement} from "../../types/sortingElements";
 
-export const reverse = (elements: TCharElement[], step: number | null = null): void => {
-    let start = step ? 0 + step : 0;
-    let end = step ? elements.length - 1 - step : elements.length - 1;
-    while (start < end) {
-        swap(elements, start, end);
-        if (step !== null) {
-            break;
-        } else {
-            start++;
-            end--;
-        }
+export const reverse = (
+  elements: TCharElement[],
+  step: number | null = null
+): void => {
+  let start = step ? 0 + step : 0;
+  let end = step ? elements.length - 1 - step : elements.length - 1;
+  while (start < end) {
+    swap(elements, start, end);
+    if (step !== null) {
+      break;
+    } else {
+      start++;
+      end--;
     }
+  }
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,12 @@
-import { ElementStates} from "../types/element-states";
+import { ElementStates } from "../types/element-states";
 
-export const swap = <T>(array: T[], i: number, j: number): void => {[array[i], array[j]] = [array[j], array[i]]};
+export const swap = <T>(array: T[], i: number, j: number): void => {
+  [array[i], array[j]] = [array[j], array[i]];
+};
 
 export const changeElementsState = (elements: any[], state: ElementStates) => {
-    elements.forEach(element => element.state = state)
-}
+  elements.forEach((element) => (element.state = state));
+};
 
-export const timeoutPromise = (timeout: number) => new Promise((resolve) => setTimeout(resolve, timeout));
+export const timeoutPromise = (timeout: number) =>
+  new Promise((resolve) => setTimeout(resolve, timeout));
