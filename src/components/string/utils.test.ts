aa9e38a3973308a720f49ddb9dd68a5705c9b820
Yury Torgashin <imaginarylight@list.ru>
2023-03-09 21:09:05 +0300

    протестировать алгоритм разворота строки

--- src/components/string/utils.test.ts
+++ src/components/string/utils.test.ts
@@ -1,5 +1,35 @@
 import { TCharElement } from "../../types/sortingElements";
+import { assembleElementsArray, reverse } from "./utils";
 
-const strings = ['testeven', 'testodd', 't', ''];
+const strings = ["testeven", "testodd", "t", ""];
 
-// const [evenChars, oddChars, oneChar, emptyString] = strings.map(string = >)
+const [evenChars, oddChars, singleChar, emptyString] = strings.map((string) =>
+  assembleElementsArray(string)
+);
+
+const assembleStringBack = (array: TCharElement[]) =>
+  array.reduce((acc, element) => (acc += element.char), "");
+
+const mock = jest.fn();
+
+describe("Algorithm", () => {
+  it("should reverse a string with an even number of chars", async function () {
+    await reverse(evenChars, mock, 0);
+    expect(assembleStringBack(evenChars)).toEqual("nevetset");
+  });
+
+  it("should reverse a string with an odd number of chars", async function () {
+    await reverse(oddChars, mock, 0);
+    expect(assembleStringBack(oddChars)).toEqual("ddotset");
+  });
+
+  it("should reverse a string with a single char", async function () {
+    await reverse(singleChar, mock, 0);
+    expect(assembleStringBack(singleChar)).toEqual("t");
+  });
+
+  it("should reverse an empty string", async function () {
+    await reverse(emptyString, mock, 0);
+    expect(assembleStringBack(emptyString)).toEqual("");
+  });
+});
